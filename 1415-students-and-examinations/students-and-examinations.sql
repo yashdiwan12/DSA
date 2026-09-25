ec8130d2f51c2d9c941157d6978ab018a90be926
Yash Diwan <diwanyash12@gmail.com>
2026-09-25 22:00:20 +0530

    Time: 1390 ms (16.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,9 @@
+SELECT s.student_id, 
+       s.student_name, 
+       su.subject_name, 
+       COUNT(e.student_id) attended_exams
+FROM Students s CROSS JOIN Subjects su LEFT JOIN Examinations e ON 
+s.student_id = e.student_id AND su.subject_name = e.subject_name
+
+GROUP BY s.student_id, s.student_name, su.subject_name
+ORDER BY s.student_id, s.student_name, su.subject_name;
